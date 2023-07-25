9672914a0f827479dea0594743dec7e08c981d16
Dominick Niehüsener <Dominick.Niehuesener@msg.group>
2023-07-25 10:12:17 +0200

    Implement Github-Workflow for building Github-Pages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: "export"
+    output: "export",
+
+    basePath: "/gbh-techradar",
+
+    images: {
+        unoptimized: true,
+    },
 }
 
 module.exports = nextConfig
